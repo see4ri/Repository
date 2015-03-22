d608d8be0abba9d1d0027b78430cc731dd436cf2
Rangarajan Rajendiran <rrangarajan.85@gmail.com>
2015-03-22 22:38:36 +0530

    Added makefiles and few programs

---
+++ install.sh
@@ -0,0 +1,76 @@
+# Note : DO THE FOLLOWING BEFORE RUNNING THE SCRIPT
+# Step 1 : Go to Ubuntu Software Center --> Edit --> Software Sources
+# Step 2 : Under Ubuntu Software tab, enable all the repositories except source code 
+#          and click on download from 
+# Step 3 : Select other from download drop down
+# Step 4 : Now click on select best server
+# Step 5 : You need to authenticate this with super user password.
+# Step 6 : Once Ubuntu select best server for you click on chose server
+# Step 7 : Now click on other software tab.
+# Step 8 : Select Canonical Partners, Canonical Partners(Source code), 
+#          Independent, Independent(source code)
+# Step 9 : And click on close.
+
+# vim 
+sudo apt-get install vim
+
+# vlc player 
+sudo apt-get install vlc
+
+# git  
+sudo apt-get install git
+
+# terminator
+sudo apt-get install terminator
+
+# file zilla
+sudo apt-get install filezilla
+
+#Secret Maryo Chronicles - Adventure
+sudo apt-get install smc
+
+#0 A.D. - Strategy
+sudo add-apt-repository ppa:wfg/0ad
+sudo apt-get update && sudo apt-get install 0ad
+
+
+#skype
+#sudo apt-get install libqt4-dbus libqt4-network libqt4-xml libasound2
+#wget http://www.skype.com/go/getskype-linux-beta-ubuntu-64
+#sudo dpkg -i getskype-*
+#sudo apt-get -f install
+
+
+# OpenShot
+#sudo apt-get update
+#sudo apt-get upgrade
+#sudo apt-get install openshot
+
+# PiTiVi
+#sudo apt-get install pitivi
+
+# calendar indicator
+#sudo add-apt-repository ppa:atareao/atareao
+#sudo apt-get update
+#sudo apt-get install calendar-indicator
+
+# variety wallpaper changer
+#sudo add-apt-repository ppa:peterlevi/ppa
+#sudo apt-get update
+#sudo apt-get install variety
+
+# Tweak Unity
+#sudo apt-get install unity-tweak-tool
+
+# Get Firefox to support H.264
+#sudo add-apt-repository ppa:mc3man/trusty-media
+#sudo apt-get update
+#sudo apt-get install gstreamer0.10-ffmpeg
+
+# Fix Skype not using the correct system theme on 64bit
+#sudo apt-get install gtk2-engines-murrine:i386 gtk2-engines-pixbuf:i386
+
+# Fix missing Skype tray icon / appindicator on Ubuntu 64bit
+#sudo apt-get install sni-qt:i386
+
+
