8087a90fd03896ae83acdaa1046cc92c1b4e7b3e
Rangarajan Rajendiran <rrangarajan.85@gmail.com>
2015-04-21 15:57:27 +0530

    Update Program01.c

--- Programs/Linux/source/Program01.c
+++ Programs/Linux/source/Program01.c
@@ -1,9 +1,13 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<unistd.h>
-
+#include<pthread.h>
+int func1()
+{
+ printf("Inside Function 1\n");
+}
 int main() {
-
+ pthread_t thread_id1, thread_id2, thread_id3;
  printf("This is program 01 \n");
  printf("The process id of parent program01 is %d\n",(int)getppid());
  printf("The process id of program01 is %d\n",(int)getpid());
