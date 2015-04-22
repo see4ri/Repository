0636fdbc2171cf6f7a1e9fe1bfcd2b6717bd5928
rangaeeeee <rrangarajan.85@gmail.com>
2015-04-22 08:36:13 +0530

    changes made in thread return

--- Programs/Linux/source/Program01.c
+++ Programs/Linux/source/Program01.c
@@ -21,10 +21,12 @@ void* func1(void* param)
  
  printf("thread name : %s\n",temp->name);
  printf("thread id : %d\n",temp->id);
+
+ temp->id += 10;
  
  printf("End Thread 1\n");
 
- return 0;
+ return (void *)temp;
 }
 
 /*This is thread 02*/
@@ -38,10 +40,12 @@ void* func2(void* param)
  
  printf("thread name : %s\n",temp->name);
  printf("thread id : %d\n",temp->id);
+
+ temp->id += 10;
  
  printf("End Thread 2\n");
  
- return 0;
+ return (void *)temp;
 }
 
 /*This is thread 03*/
@@ -56,15 +60,18 @@ void* func3(void* param)
  printf("thread name : %s\n",temp->name);
  printf("thread id : %d\n",temp->id);
  
+ temp->id += 10;
+
  printf("End Thread 3\n");
  
- return 0;
+ return (void *)temp;
 }
 
 int main() {
  pthread_t thread_id1, thread_id2, thread_id3;
  struct threadData thread_arg1, thread_arg2, thread_arg3;
- 
+ struct threadData *pthread_arg1, *pthread_arg2, *pthread_arg3;
+  
  printf("Start of Process 01 \n");
  printf("The process id of parent Process 01 is %d\n",(int)getppid());
  printf("The process id of Process 01 is %d\n",(int)getpid());
@@ -78,11 +85,21 @@ int main() {
  strcpy(thread_arg3.name,"Thread 03");
  thread_arg3.id   = 03;
  
- 
+ pthread_arg1 = &thread_arg1;
+ pthread_arg2 = &thread_arg2;
+ pthread_arg3 = &thread_arg3;
+
  pthread_create(&thread_id1,NULL,&func1,&thread_arg1);
  pthread_create(&thread_id2,NULL,&func2,&thread_arg2);
  pthread_create(&thread_id3,NULL,&func3,&thread_arg3);
- 
+
+ pthread_join(thread_id1,(void*)&pthread_arg1);
+ pthread_join(thread_id2,(void*)&pthread_arg2);
+ pthread_join(thread_id3,(void*)&pthread_arg3);
+
+ printf("The return value of thread 1 : %d\n",thread_arg1.id);
+ printf("The return value of thread 2 : %d\n",thread_arg2.id);
+ printf("The return value of thread 3 : %d\n",thread_arg3.id);
  printf("End of Proicess 01 \n");
 
  return 0;
--- Programs/Linux/source/Program02.c
+++ Programs/Linux/source/Program02.c
@@ -22,9 +22,11 @@ void* func1(void* param)
  printf("thread name : %s\n",temp->name);
  printf("thread id : %d\n",temp->id);
  
+ temp->id += 10;
+
  printf("End Thread 4\n");
 
- return 0;
+ return (void *)temp;
 }
 
 /*This is thread 05*/
@@ -38,10 +40,12 @@ void* func2(void* param)
  
  printf("thread name : %s\n",temp->name);
  printf("thread id : %d\n",temp->id);
+
+ temp->id += 10;
  
  printf("End Thread 5\n");
  
- return 0;
+ return (void *)temp;
 }
 
 /*This is thread 06*/
@@ -55,16 +59,24 @@ void* func3(void* param)
  
  printf("thread name : %s\n",temp->name);
  printf("thread id : %d\n",temp->id);
+
+ temp->id += 10;
  
  printf("End Thread 6\n");
  
- return 0;
+ return (void *)temp;
 }
 
 int main() {
  pthread_t thread_id4, thread_id5, thread_id6;
  struct threadData thread_arg4, thread_arg5, thread_arg6;
  
+ struct threadData *pthread_arg4, *pthread_arg5, *pthread_arg6;
+ 
+ pthread_arg4 = &thread_arg4;
+ pthread_arg5 = &thread_arg5;
+ pthread_arg6 = &thread_arg6;
+
  printf("Start of Process 02 \n");
  printf("The process id of parent Process 02 is %d\n",(int)getppid());
  printf("The process id of Process 02 is %d\n",(int)getpid());
@@ -82,6 +94,15 @@ int main() {
  pthread_create(&thread_id4,NULL,&func1,&thread_arg4);
  pthread_create(&thread_id5,NULL,&func2,&thread_arg5);
  pthread_create(&thread_id6,NULL,&func3,&thread_arg6);
+
+ pthread_join(thread_id4,(void *)&pthread_arg4);
+ pthread_join(thread_id5,(void *)&pthread_arg5);
+ pthread_join(thread_id6,(void *)&pthread_arg6);
+
+ 
+ printf("The value returned by thread 4 is %d\n",thread_arg4.id);
+ printf("The value returned by thread 5 is %d\n",thread_arg5.id);
+ printf("The value returned by thread 6 is %d\n",thread_arg6.id);
  
  printf("End of Process 02 \n");
 
