fbbe9d061f774262e2d3b23dff141bb4038e13dd
rangaeeeee <rrangarajan.85@gmail.com>
2015-05-24 21:47:04 +0530

    changed to small letters

--- programs/miniprojects/carparking/src/EntrySystem.c
+++ programs/miniprojects/carparking/src/EntrySystem.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <string.h>
+#include <time.h>
+
+/* Defines */
+#define STRING_MAX 50 /* Maximum length of the string */
+
+/* Thread Data*/
+struct Data {
+    char carNumber[STRING_MAX];
+    long ownerTokenID;
+    struct tm time;
+};
 
 int main (int argc,char* argv[]) {
+    pthread_t thread_id1, thread_id2, thread_id3;
+    time_t timer;
+
+    printf(" Current timing of the process is %s \n",asctime(localtime(&timer)));
 
     printf("Inside Entry system process \n");
 
